57d0e67a1e404ea382c440c25c2de1dd4f094f2d
AlishaKausheen <alishakausheen@gmail.com>
2024-10-07 23:00:07 +0530

    final changes in authentication part ui

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,9 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 function DashboardLayout(){
     return (
         <div>
-            DashboardLayout
+            <UserButton/>
         </div>
     )
 }
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function Dashboard({children}) {
   return (
     <div>
       {children}
+      
     </div>
   )
 }

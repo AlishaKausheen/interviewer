8baf2fe58d24208b0ce10805059fb9db7d091e79
AlishaKausheen <alishakausheen@gmail.com>
2024-10-21 20:52:21 +0530

    interview landing page

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -5,7 +5,7 @@ import { db } from '../../../../utils/db';
 import { MockInterview } from '../../../../utils/schema';
 import { eq } from 'drizzle-orm';
 import Webcam from 'react-webcam';
-import { WebcamIcon } from 'lucide-react';
+import { Lightbulb, WebcamIcon } from 'lucide-react';
 import {Button} from '../../../../components/button'
 function Interview({params}) {
     const [interviewData, setInterviewData] = useState();
@@ -16,15 +16,28 @@ function Interview({params}) {
     },[])
     // used to get interview by mockid/interviewid
     const GetInterviewDetails=async ()=>{
-        const result = await db.select().from(MockInterview)
-        .where(eq(MockInterview.mockId,params.interviewId));
+        const result = await db.select().from(MockInterview).where(eq(MockInterview.mockId,params.interviewId));
 
-        //console.log(result);
+    //console.log(result);
         setInterviewData(result[0]);
+        console.log(result[0]);
     }
   return (
-    <div className='my-10 flex justify-center flex-col items-center'>
+    <div className='my-10 '>
       <h2 className='font-bold text-2xl'>Let's get Started</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+      
+      <div className='flex flex-col my-5 gap-5 '>
+        <div className='flex flex-col gap-5 p-5 rounded-lg border'>
+        <h2 className='text-lg'><strong>Job Role/Job Position: </strong>{interviewData?.jobPosition}</h2>
+        <h2 className='text-lg'><strong>Job Description/Tech Stacks: </strong>{interviewData?.jobDesc}</h2>
+        <h2 className='text-lg'><strong>Years of Experience: </strong>{interviewData?.jobExperience}</h2>
+      </div>
+     <div className='p-5 border rounded-lg border-yellow-300 bg-yellow-100'>
+      <h2 className='flex gap-2 items-center text-yellow-500'><Lightbulb/><strong>Information</strong></h2>
+      <h2 className='mt-3 text-yellow-500'>{process.env.NEXT_PUBLIC_INFORMATION}</h2>
+     </div>
+      </div>
       <div>
        {webcamEnabled? <Webcam 
        onUserMedia={()=>setWebcamEnabled(true)}
@@ -36,13 +49,15 @@ function Interview({params}) {
        }}/>
        :<>
         <WebcamIcon className='h-72 w-full my-7 p-20 bg-secondary rounded-lg border'/>
-        <Button onClick={()=>setWebcamEnabled(true)}>Enable webcam and microphone</Button>
+        <Button variant="ghost" className="w-full" onClick={()=>setWebcamEnabled(true)}>Enable webcam and microphone</Button>
         </>
        }
       </div>
-      <div>
-        <h2><strong>Job Description</strong></h2>
       </div>
+      <div className='flex justify-end items-end'>
+            <Button>Start Interview</Button>
+            </div>
+
     </div>
   )
 }

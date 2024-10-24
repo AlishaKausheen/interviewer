75674387f46389ad8bf8fa4e81a899d976046e50
AlishaKausheen <alishakausheen@gmail.com>
2024-10-24 23:19:15 +0530

    questin component done

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -5,12 +5,15 @@ import React, { useEffect, useState } from 'react'
 import { db } from '../../../../../utils/db';
 import { MockInterview } from '../../../../../utils/schema';
 import { eq } from 'drizzle-orm';
+import { Lightbulb } from 'lucide-react';
+import Webcam from 'react-webcam';
 //import {QuestionsSection} from "./_components/QuestionsSection"
 //import {QuestionsSection} from './_components/QuestionsSection'
 function StartInterview({params}) {
 
   const [interviewData,setInterviewData] = useState();
   const [mockInterviewQuestion, setMockInterviewQuestion] = useState();
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   useEffect(()=>{
 
     GetInterviewDetails();
@@ -31,11 +34,24 @@ setInterviewData(result[0]);
       <div className='grid grid-cols-1 md:grid-cols-2'>
         
 {/*questions */}
-<div>
+<div className='p-5 border rounded-lg my-5'>
+  <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
   {mockInterviewQuestion && mockInterviewQuestion.map((question,index)=>(
-    <h2>Question #{index+1}</h2>
+    <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex==index&&'bg-blue-800 text-white'}`}>Question #{index+1}</h2>
   ))}
-  </div>       
+ 
+  </div>  
+  <h2 className='my-5 text-md md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
+  <div className='border rounded-lg p-5 bg-blue-100 mt-20'>
+    <h2 className='flex gap-2 items-center text-primary'>
+      <Lightbulb/>
+      <strong>Note:</strong>
+    </h2>
+    <h2 className='text-sm text-primary my-2'>{process.env.NEXT_PUBLIC_INFORMATION}</h2>
+  </div>
+  </div> 
+  {/* audio and video recording */}
+    <Webcam/>
       </div>
 
     </div>

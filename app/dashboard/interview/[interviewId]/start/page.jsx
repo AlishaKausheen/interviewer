8e38f7c367835bb956d6f8842063f457e0305f63
AlishaKausheen <alishakausheen@gmail.com>
2024-10-25 21:22:53 +0530

    improving style of record answer

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -7,6 +7,9 @@ import { MockInterview } from '../../../../../utils/schema';
 import { eq } from 'drizzle-orm';
 import { Lightbulb } from 'lucide-react';
 import Webcam from 'react-webcam';
+import Image from 'next/image';
+import { Button } from '../../../../../components/button';
+
 //import {QuestionsSection} from "./_components/QuestionsSection"
 //import {QuestionsSection} from './_components/QuestionsSection'
 function StartInterview({params}) {
@@ -31,17 +34,17 @@ setInterviewData(result[0]);
 }
   return (
     <div>
-      <div className='grid grid-cols-1 md:grid-cols-2'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-1'>
         
 {/*questions */}
 <div className='p-5 border rounded-lg my-5'>
   <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
   {mockInterviewQuestion && mockInterviewQuestion.map((question,index)=>(
-    <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex==index&&'bg-blue-800 text-white'}`}>Question #{index+1}</h2>
+    <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex==index&&'bg-blue-900 text-white'}`}>Question #{index+1}</h2>
   ))}
- 
   </div>  
   <h2 className='my-5 text-md md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
+
   <div className='border rounded-lg p-5 bg-blue-100 mt-20'>
     <h2 className='flex gap-2 items-center text-primary'>
       <Lightbulb/>
@@ -51,7 +54,20 @@ setInterviewData(result[0]);
   </div>
   </div> 
   {/* audio and video recording */}
-    <Webcam/>
+  <div className='flex items-center justify-center flex-col'>
+      <div className='flex flex-col mt-20 justify-center items-center bg-black  rounded-lg p-5'>
+  <Image src={'/webcam.png'} width={200} height={200} className='absolute'/>
+    <Webcam
+    mirrored={true}
+    style={{
+      height:300,
+      width:'100%',
+      zIndex:10,
+    }}
+    />
+    </div>
+<Button variant='outline' className='my-10'>Record Answer</Button>
+    </div>
       </div>
 
     </div>

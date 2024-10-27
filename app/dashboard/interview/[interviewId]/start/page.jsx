62a65f18d04e4198f98933ceabd38ba229d56a30
AlishaKausheen <alishakausheen@gmail.com>
2024-10-27 22:26:58 +0530

    feature to record and view answer

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -5,18 +5,22 @@ import React, { useEffect, useState } from 'react'
 import { db } from '../../../../../utils/db';
 import { MockInterview } from '../../../../../utils/schema';
 import { eq } from 'drizzle-orm';
-import { Lightbulb } from 'lucide-react';
+import { Lightbulb, Mic, Volume2 } from 'lucide-react';
 import Webcam from 'react-webcam';
 import Image from 'next/image';
 import { Button } from '../../../../../components/button';
+import useSpeechToText from 'react-hook-speech-to-text';
+
 
 //import {QuestionsSection} from "./_components/QuestionsSection"
 //import {QuestionsSection} from './_components/QuestionsSection'
 function StartInterview({params}) {
 
   const [interviewData,setInterviewData] = useState();
-  const [mockInterviewQuestion, setMockInterviewQuestion] = useState();
+  const [mockInterviewQuestion, setMockInterviewQuestion] = useState([]);
   const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [userAnswer, setUserAnswer] = useState('');
+  const [showAnswer, setShowAnswer] = useState(false);
   useEffect(()=>{
 
     GetInterviewDetails();
@@ -32,6 +36,33 @@ setMockInterviewQuestion(jsonMockResp);
 setInterviewData(result[0]);
 
 }
+
+
+const {
+  error,
+  interimResult,
+  isRecording,
+  results,
+  startSpeechToText,
+  stopSpeechToText,
+} = useSpeechToText({
+  continuous: true,
+  useLegacyResults: false
+});
+
+const textToSpeech=(text)=>{
+  if('speechSynthesis' in window){
+    const speech =new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(speech);
+  }else{
+    alert('Sorry, your browser doesnot support text to speech')
+  }
+}
+useEffect(()=>{
+results.map((result)=>(
+  setUserAnswer(prevAns=> prevAns+result?.transcript)
+))
+},[results])
   return (
     <div>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-1'>
@@ -40,11 +71,22 @@ setInterviewData(result[0]);
 <div className='p-5 border rounded-lg my-5'>
   <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
   {mockInterviewQuestion && mockInterviewQuestion.map((question,index)=>(
-    <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex==index&&'bg-blue-900 text-white'}`}>Question #{index+1}</h2>
+    <h2  key={index}
+    onClick={() => setActiveQuestionIndex(index)} className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex==index&&'bg-blue-900 text-white'}`}>Question #{index+1}</h2>
   ))}
   </div>  
-  <h2 className='my-5 text-md md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
 
+  <h2 className='my-5 text-md md:text-lg'>
+  {mockInterviewQuestion.length > 0 
+    ? mockInterviewQuestion[activeQuestionIndex]?.question 
+    : "Loading questions..."}
+</h2>
+<Volume2 className='cursor-pointer' onClick={()=> textToSpeech(mockInterviewQuestion[activeQuestionIndex]?.question)}/>
+{showAnswer && (
+        <div className="mt-2 p-2 bg-gray-200 rounded">
+        Answer:  {userAnswer || 'No answer provided.'}
+        </div>
+      )}
   <div className='border rounded-lg p-5 bg-blue-100 mt-20'>
     <h2 className='flex gap-2 items-center text-primary'>
       <Lightbulb/>
@@ -66,10 +108,16 @@ setInterviewData(result[0]);
     }}
     />
     </div>
-<Button variant='outline' className='my-10'>Record Answer</Button>
+<Button variant='outline' className='my-10'
+onClick={isRecording?stopSpeechToText:startSpeechToText}>
+  {isRecording? <h2 className='text-red-600 flex gap-2'><Mic/>Stop Recording</h2>:'Record Answer'}
+  </Button>
+<Button onClick={() => setShowAnswer(!showAnswer)}>Show User Answer</Button>
     </div>
       </div>
 
+
+     
     </div>
   )
 }
